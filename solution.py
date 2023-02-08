1f8ff80e808a628de249e7ff8cd44de8dc9ccf85
Pavel Filatov <filatov.py@gmail.com>
2023-02-08 23:08:22 +0100

    [01] Implement solution

--- solution.py
+++ solution.py
@@ -1,5 +1,36 @@
-from typing import Dict
+import logging
+import re
+from typing import Dict, Optional
+
+SEPARATOR = r"[\s,]+"
+DEFAULT_HOUSE_NUMBER = r"\d+(\s*[a-z]\b)?"
+STREET = r"[\w\s\.]+"
+
+extended_house_number = r"\bno\s*{default_house_number}".format(default_house_number=DEFAULT_HOUSE_NUMBER)
+
+street_housenum = r"^(?P<street>{street}){sep}(?P<housenumber>{house_number})$"
+housenum_street = r"^(?P<housenumber>{house_number}){sep}(?P<street>{street})$"
+
+patterns_raw = [
+    (
+        "starts_with_extensive_house",
+        housenum_street.format(street=STREET, sep=SEPARATOR, house_number=extended_house_number),
+    ),
+    (
+        "starts_with_extensive_street",
+        street_housenum.format(street=STREET, sep=SEPARATOR, house_number=extended_house_number),
+    ),
+    ("starts_with_house", housenum_street.format(street=STREET, sep=SEPARATOR, house_number=DEFAULT_HOUSE_NUMBER)),
+    ("starts_with_street", street_housenum.format(street=STREET, sep=SEPARATOR, house_number=DEFAULT_HOUSE_NUMBER)),
+]
+
+compiled_patterns = [(pattern_name, re.compile(pattern, re.IGNORECASE)) for pattern_name, pattern in patterns_raw]
 
 
-def parse_address(address_string: str) -> Dict[str, str]:
+def parse_address(address_string: str) -> Optional[Dict[str, str]]:
     """Extracts street name and house number from a string."""
+    for pattern_name, pattern in compiled_patterns:
+        m = re.match(pattern, address_string)
+        if m:
+            logging.info(f"String{address_string}\tMatched pattern '{pattern_name}'")
+            return m.groupdict()
--- test_solution.py
+++ test_solution.py
@@ -8,16 +8,32 @@ from solution import parse_address
 @pytest.mark.parametrize(
     argnames=["address_string", "expected"],
     argvalues=(
-            ("Winterallee 3", {"street": "Winterallee", "housenumber": "3"}),
-            ("Musterstrasse 45", {"street": "Musterstrasse", "housenumber": "45"}),
-            ("Blaufeldweg 123B", {"street": "Blaufeldweg", "housenumber": "123B"}),
-            ("Am Bächle 23", {"street": "Am Bächle", "housenumber": "23"}),
-            ("Auf der Vogelwiese 23 b", {"street": "Auf der Vogelwiese", "housenumber": "23 b"}),
-            ("4, rue de la revolution", {"street": "rue de la revolution", "housenumber": "4"}),
-            ("200 Broadway Av", {"street": "Broadway Av", "housenumber": "200"}),
-            ("Calle Aduana, 29", {"street": "Calle Aduana", "housenumber": "29"}),
-            ("Calle 39 No 1540", {"street": "Calle 39", "housenumber": "No 1540"}),
-    )
+        # Simple cases
+        ("Winterallee 3", {"street": "Winterallee", "housenumber": "3"}),
+        ("Musterstrasse 45", {"street": "Musterstrasse", "housenumber": "45"}),
+        ("Blaufeldweg 123B", {"street": "Blaufeldweg", "housenumber": "123B"}),
+
+        # More complicated ones
+        ("Am Bächle 23", {"street": "Am Bächle", "housenumber": "23"}),
+        ("Auf der Vogelwiese 23 b", {"street": "Auf der Vogelwiese", "housenumber": "23 b"}),
+
+        # Complex cases, incl. other countries
+        ("4, rue de la revolution", {"street": "rue de la revolution", "housenumber": "4"}),
+        ("200 Broadway Av", {"street": "Broadway Av", "housenumber": "200"}),
+        ("Calle Aduana, 29", {"street": "Calle Aduana", "housenumber": "29"}),
+        ("Calle 39 No 1540", {"street": "Calle 39", "housenumber": "No 1540"}),
+
+        # Edge cases
+        ## No space in "No13"
+        ("No13 Paulsternstr.", {"street": "Paulsternstr.", "housenumber": "No13"}),
+        ## More than a single character after the number
+        ("12Unter den Linden", None),
+        ## But it works if there is only 1 character after a number
+        ("12A Unter den Linden", {"street": "Unter den Linden", "housenumber": "12A"}),
+
+        # What if there is only a street? Now we return nothing but is it correct?
+        ("Unter den Linden", None),
+    ),
 )
 def test_parse_address(address_string: str, expected: Dict[str, str]):
     assert parse_address(address_string) == expected
